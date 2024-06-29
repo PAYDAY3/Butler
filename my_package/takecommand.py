7d8ba2d3d594c415391be777907b54c57b83c0b7
PAYDAY3 <1484533583@qq.com>
2024-06-29 10:46:56 +0800

    Update takecommand.py

--- my_package/takecommand.py
+++ my_package/takecommand.py
@@ -1,36 +1,37 @@
-from pocketsphinx import LiveSpeech
 import subprocess
 import speech_recognition as sr
 
+model = "my_Snowboy/jarvis.umdl"  # Snowboy 模型文件路径
+
 def setup_recognizer():
-    recognizer = sr.Recognizer()
-    recognizer.pause_threshold = 1  # 静态能量阈值
-    recognizer.dynamic_energy_adjustment_damping = 0.15  # 调整阈值的平滑因子
-    recognizer.dynamic_energy_ratio = 1.5  # 阈值调整比率
-    recognizer.operation_timeout = 5  # 最长等待时间（秒）
-    recognizer.energy_threshold = 4000  # 设置音量阈值
-    recognizer.dynamic_energy_threshold = True  # 自动调整音量阈值
-    return recognizer
-
-def takecommand():
-    recognizer = setup_recognizer()
-
-    # 设置自定义语音识别模型文件的路径
-    language_model_path = "cmusphinx-zh-cn/zh_cn.lm.bin"
-    dictionary_path = "cmusphinx-zh-cn/zh_cn.dict"
-    
-    # 设置模型文件路径
-    recognizer.default = 'my_snowboy/jarvis.umdl'
-
+    recognizer = sr.Recognizer()   # 初始化语音识别器和文本到语音引擎
+    global recognizer
     with sr.Microphone() as source:
         print("请说话...")
+        recognizer.pause_threshold = 1  # 静态能量阈值
+        recognizer.dynamic_energy_adjustment_damping = 0.15 # 调整阈值的平滑因子
+        recognizer.dynamic_energy_ratio = 1.5  # 阈值调整比率
+        recognizer.operation_timeout = 5  # 最长等待时间（秒）
+        recognizer.energy_threshold = 4000      # 设置音量阈值
+        recognizer.dynamic_energy_threshold = True  # 自动调整音量阈值
+        recognizer.default = model
         audio = recognizer.listen(source, phrase_time_limit=5)
-
-    try:
-        text = recognizer.recognize_sphinx(audio, language='zh-CN', language_model_path, dictionary_path)  # 使用Sphinx引擎进行本地识别
-        print("识别结果: " + text)
-        # 在这里添加根据识别结果执行其他操作的逻辑
-    except sr.UnknownValueError:
-        print("无法识别，请重试")
-    except sr.RequestError as e:
-        print("请求出错：{0}".format(e))
+        # 将录制的音频保存为临时文件
+        with tempfile.NamedTemporaryFile(delete=False) as f:
+            audio_file_path = f.name + ".wav"
+            f.write(audio.get_wav_data())
+    return audio_file_path 
+        try:
+            print("Recognizing...")  # 识别中...
+            query = recognizer.recognize_sphinx(audio, language='zh-CN')
+            print('User: ' + query + '\n')
+            return query     
+        except Exception as e:
+            print("对不起，我没有听清楚，请再说一遍。")
+            speak("对不起，我没有听清楚，请再说一遍。")
+            query = None
+            return query
+        except sr.RequestError as error:
+            print(f"语音识别请求出错：{error}")
+            logging.error(f"语音识别请求出错：{error}")
+            return ""
