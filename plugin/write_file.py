962c12cdc6ed37256ff146d002f86bc7b9f4613b
PAYDAY3 <1484533583@qq.com>
2024-07-17 18:49:49 +0800

    Update write_file.py

--- plugin/write_file.py
+++ plugin/write_file.py
@@ -37,9 +37,16 @@ class WriteFilePlugin(AbstractPlugin):
             "required": ["file_path", "content"],
         }
 
-    def run(self, takecommand, args: dict) -> PluginResult:
+def run(self, takecommand: str, args: dict) -> PluginResult:
+    if takecommand is None:
+        return PluginResult.new("没有检测到语音指令", need_call_brain=False)
+
+    content = args.get("content")
+    if content:
         file_name = f"write_file-{str(int(time.time()))}.md"
-        file_path = os.path.abspath(os.path.join(system_config.TEMP_DIR_PATH, file_name))
+        file_path = os.path.abspath(os.path.join(TEMP_DIR_PATH, file_name))
         with open(file_path, "w") as f:
-            f.write(args.get("content"))
-        return PluginResult.new(result="已将内容写入到文件【{}】中。".format(file_path), need_call_brain=True)
+            f.write(content)
+        return PluginResult.new(result=f"已将内容写入到文件【{file_path}】中。", need_call_brain=True)
+    else:
+        return PluginResult.new(result="写入内容不能为空。", need_call_brain=False)
