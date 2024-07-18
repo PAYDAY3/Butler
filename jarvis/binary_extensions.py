718133de64cb6db9e897c175255bed48cf191567
PAYDAY3 <1484533583@qq.com>
2024-07-18 18:13:04 +0800

    Create binary_extensions.py

---
+++ jarvis/binary_extensions.py
@@ -0,0 +1,38 @@
+binary_extensions = [
+        # 编程语言
+        '.py', '.c', 'cs', '.cpp', '.java', '.sh', 'go', '.js', '.html', '.css', '.bat', '.asm',
+        # 编译的可执行文件和库
+        '.exe', '.dll', '.so', '.a', '.lib', '.dylib', '.o', '.obj',
+        # 压缩档案
+        '.zip', '.tar', '.tar.gz', '.tgz', '.rar', '.7z', '.bz2', '.gz', '.xz', '.z', '.lz', '.lzma', '.lzo', '.rz', '.sz', '.dz',
+        # 特定于应用程序的文件
+        '.pdf', '.doc', '.docx', '.xls', '.xlsx', '.ppt', '.pptx', '.odt', '.ods', '.odp',
+        # 媒体文件(不常见)
+        '.png', '.jpg', '.jpeg', '.gif', '.mp3', '.mp4', '.wav', '.flac', '.ogg', '.avi', '.mkv', '.mov', '.webm', '.wmv', '.m4a', '.aac',
+        # 虚拟机和容器映像
+        '.iso', '.vmdk', '.qcow2', '.vdi', '.vhd', '.vhdx', '.ova', '.ovf',
+        # 数据库文件
+        '.db', '.sqlite', '.mdb', '.accdb', '.frm', '.ibd', '.dbf',
+        # 与java相关的文件
+        '.jar', '.class', '.war', '.ear', '.jpi',
+        # Python字节码和包
+        '.pyc', '.pyo', '.pyd', '.egg', '.whl',
+        # 其他潜在的重要扩展
+        '.deb', '.rpm', '.apk', '.msi', '.dmg', '.pkg', '.bin', '.dat', '.data',
+        '.dump', '.img', '.toast', '.vcd', '.crx', '.xpi', '.lockb', 'package-lock.json', '.svg' ,
+        '.eot', '.otf', '.ttf', '.woff', '.woff2',
+        '.ico', '.icns', '.cur',
+        '.cab', '.dmp', '.msp', '.msm',
+        '.keystore', '.jks', '.truststore', '.cer', '.crt', '.der', '.p7b', '.p7c', '.p12', '.pfx', '.pem', '.csr',
+        '.key', '.pub', '.sig', '.pgp', '.gpg',
+        '.nupkg', '.snupkg', '.appx', '.msix', '.msp', '.msu',
+        '.deb', '.rpm', '.snap', '.flatpak', '.appimage',
+        '.ko', '.sys', '.elf',
+        '.swf', '.fla', '.swc',
+        '.rlib', '.pdb', '.idb', '.pdb', '.dbg',
+        '.sdf', '.bak', '.tmp', '.temp', '.log', '.tlog', '.ilk',
+        '.bpl', '.dcu', '.dcp', '.dcpil', '.drc',
+        '.aps', '.res', '.rsrc', '.rc', '.resx',
+        '.prefs', '.properties', '.ini', '.cfg', '.config', '.conf',
+        '.DS_Store', '.localized', '.svn', '.git', '.gitignore', '.gitkeep',
+    ]
