369fed9c9caadb9f7afecf242560cf7fffc7d64f
PAYDAY3 <1484533583@qq.com>
2024-08-11 10:12:22 +0800

    Update e-mail.py

--- my_package/e-mail.py
+++ my_package/e-mail.py
@@ -13,66 +13,23 @@ import smtplib
 from email.mime.text import MIMEText
 from email.header import Header
 from email.mime.multipart import MIMEMultipart
-from email.mime.application import MIMEApplication
-from jarvis.jarvis import takecommand
-from plugin.plugin_interface import AbstractPlugin, PluginResult
 
 your_password = "your_password"  # 请替换为您的实际密码
 Email_address = "example@qq.com"  # 请替换为您的实际邮箱地址
 
-class Plugin(AbstractPlugin):
+class Plugin():
 
     SLUG = "email"
     
-    def valid(self) -> bool:
-        return True
-        
     def __init__(self):
         self.load_config()
         self.current_account_index = 0  # 默认使用第一个账户
         self.update_account_info()  
-        self._logger = None
           
     def load_config(self):
         with open("./email_config.json", "r") as f:
             self.config = json.load(f)
             
-    def init(self, logger: logging.Logger):
-        self._logger = logger
-         
-    def get_name(self):
-        return "send_email"
-
-    def get_chinese_name(self):
-        return "发送电子邮件"
-
-    def get_description(self):
-        return "发送电子邮件的接口。"      
-             
-    def get_parameters(self):
-        return {
-            "type": "object",
-            "properties": {
-                "receiver_email": {
-                    "type": "string",
-                    "description": "收件人邮箱地址",
-                },
-                "subject": {
-                    "type": "string",
-                    "description": "邮件主题",
-                },
-                "message": {
-                    "type": "string",
-                    "description": "邮件内容",
-                },
-                "attachment_path": {
-                    "type": "string",
-                    "description": "邮件附件文件地址，如果你需要将生成的文件通过邮件发送出去，你应该使用本字段。",
-                },
-            },
-            "required": ["receiver_email", "subject", "message"],
-        }             
-        
     def update_account_info(self):
         account = self.config["accounts"][self.current_account_index]
         self.email = account["email"]
@@ -172,7 +129,7 @@ class Plugin(AbstractPlugin):
                 print(f"附件已保存：{filepath}")                         
 
     def fetchUnreadEmails(self, since=None, markRead=False, limit=None):
-        logger = Logging.getLogger(__name__)
+        logger = logging.getLogger(__name__)
         profile = config.get()
         conn = imaplib.IMAP4(
             profile[self.SLUG]["imap_server"], profile[self.SLUG]["imap_port"]
@@ -240,13 +197,7 @@ class Plugin(AbstractPlugin):
     def isValid(self, text, parsed):
         return any(word in text for word in ["邮箱", "邮件"])
         
-    def send_email(self, takecommand, subject, message, receiver, args: dict) -> PluginResult:
-        receiver_email = args.get("receiver_email")
-        subject = args.get("subject")
-        message = args.get("message")
-        attachment_path = args.get("attachment_path")
-        
-        # 创建一个MIMEMultipart对象，添加邮件内容和头部信息
+    def send_email(self, subject, message, receiver):
         msg = MIMEText(message)
         msg['Subject'] = subject
         msg['From'] = Email_address  # 发件人地址
@@ -258,44 +209,27 @@ class Plugin(AbstractPlugin):
         smtp_server = 'smtp.qq.com'  # 修改为你的SMTP服务器地址
         smtp_port = 587  # 一般情况下使用587端口
         
-        # 添加附件
-        if attachment_path is not None and attachment_path != "":
-            with open(attachment_path, "rb") as attachment:
-                part = MIMEApplication(attachment.read(), Name=attachment_path)
-            part['Content-Disposition'] = f'attachment; filename="{attachment_path}"'
-            msg.attach(part)        
-            
         # 登录 SMTP 服务器并发送邮件
         try:
             server = smtplib.SMTP(smtp_server, smtp_port)
             server.starttls()  # 开启安全传输模式（TLS）
             server.login(Email_address, your_password)  # 修改为你的邮箱密码
             server.sendmail(Email_address, [recipient_email], msg.as_string())
-            server.ehlo()
-            server.close()
-            return PluginResult.new(result="邮件发送成功", need_call_brain=True)
+            print("邮件发送成功！")
         except Exception as e:
-            self._logger.error("发送邮件失败，异常: {}".format(e))
-            return PluginResult.new(result="邮件发送失败", need_call_brain=True)
+            print("邮件发送失败:", e)
         finally:
             server.quit()
-            
-    def edit_email_content(self, content):
-        """编辑邮件内容"""
-        # 使用 textwrap 格式化文本
-        content = textwrap.fill(content, width=80)
-        # 获取用户输入的编辑内容
-        edited_content = input(f"编辑邮件内容:\n{content}\n>>> ")
-        return edited_content
         
 def config_get():
     with open("./email_config.json", "r") as f:
         config = json.load(f)
     return config
-
+    
 # 实例化插件对象
 email_plugin = Plugin()
 email_plugin.switch_account(1)
+email_plugin.switch_account(2)
 # 要发送的邮件内容
 email_subject = input("主题：")
 email_message = input("内容：")
@@ -304,8 +238,5 @@ print(email_subject)
 print(email_message)
 print(recipient_email)
 
-# 编辑邮件内容
-edited_message = email_plugin.edit_email_content(formatted_message)
-print(edited_message)
 # 调用发送邮件函数
 send_email(email_subject, email_message, Email_address, recipient_email)
