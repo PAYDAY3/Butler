335bbdaedd6090d03a430adca87a7e25f1c198dc
PAYDAY3 <1484533583@qq.com>
2024-08-15 10:47:04 +0800

    删除不必要的缩进

--- plugin/PluginManager.py
+++ plugin/PluginManager.py
@@ -21,22 +21,22 @@ class PluginManager:
         self.load_all_plugins()
         
     # 动态加载单个插件
-    def load_plugin(self, module_name, class_name: str) -> Optional[AbstractPlugin]: 
+    def load_plugin(self, module_name, class_name: str) -> Optional[AbstractPlugin]:
         try:
             module = importlib.import_module(module_name)
             # plugin_class = getattr(module, class_name)  
             plugin_class: Type[AbstractPlugin] = getattr(module, class_name)
             plugin_instance = plugin_class()
-            if plugin_instance.valid(): 
+            if plugin_instance.valid():
                 plugin_instance.init(self.logger)
                 self.plugins[plugin_instance.get_name()] = plugin_instance
                 self.logger.info(f"已加载插件: {plugin_instance.get_name()}")
-                return plugin_instance 
+                return plugin_instance
             else:
                 self.logger.warning(f"插件 {plugin_instance.get_name()} 无效.")
                 return None
         except (ModuleNotFoundError, AttributeError) as e:
-            self.logger.error(f"加载插件 {module_name}.{class_name} 失败: {e}") 
+            self.logger.error(f"加载插件 {module_name}.{class_name} 失败: {e}")
             return None
 
     # 获取插件
@@ -67,14 +67,14 @@ class PluginManager:
                     attribute = getattr(module, attribute_name)
                     if inspect.isclass(attribute) and issubclass(attribute, AbstractPlugin) and not inspect.isabstract(attribute):
                         if attribute.__name__.endswith("Plugin"):
-                            self.load_plugin(module_name, attribute.__name__) 
+                            self.load_plugin(module_name, attribute.__name__)
         return list(self.plugins.values())
 
     # 运行插件
-    def run_plugin(self, name, takecommand：str, args: List[str]) -> Optional[str]:
-        # plugin = self.get_plugin(name)
-        plugin_name = takecommand.split()[0]  # 假设插件名称在命令的开头
-        plugin = self.plugins.get(plugin_name)
+    def run_plugin(self, name, takecommand: str, args: List[str]) -> Optional[str]:
+        plugin = self.get_plugin(name)
+        # plugin_name = takecommand.split()[0]  # 假设插件名称在命令的开头
+        # plugin = self.plugins.get(plugin_name)
         if plugin:
             self.logger.info(f"正在运行插件: {name}")
             result = plugin.run(takecommand, args)
